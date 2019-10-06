c3accbe46d4b6e48443df7a25d80a0fd88380d24
Ziphil <yfe23457@nifty.com>
2019-10-07 00:29:39 +0900

    リファクタリング

--- source/zenmath/builder.rb
+++ source/zenmath/builder.rb
@@ -9,16 +9,6 @@ include REXML
 
 module ZenmathBuilder
 
-  CREATION_METHODS = {
-    :paren => :paren,
-    :integral => :integral,
-    :function => :function, :operator => :operator,
-    "n" => :custom_number, "i" => :custom_identifier, "op" => :custom_identifier, "o" => :custom_operator,
-    "text" => :raw_text,
-    "sp" => :superscript, "sb" => :superscript, "sbsp" => :subsuperscript,
-    "frac" => :fraction,
-    "sqrt" => :radical
-  }
   DATA_PATH = "resource/math.json"
   DATA = JSON.parse(File.read(File.expand_path("../" + DATA_PATH, __FILE__)))
 
@@ -26,20 +16,60 @@ module ZenmathBuilder
 
   def create_math_element(name, attributes, children_list)
     nodes = Nodes[]
-    method_name = nil
-    if DATA["paren"].key?(name)
-      method_name = CREATION_METHODS[:paren]
-    elsif DATA["integral"].key?(name)
-      method_name = CREATION_METHODS[:integral]
-    elsif DATA["function"].include?(name)
-      method_name = CREATION_METHODS[:function]
-    elsif DATA["operator"].key?(name)
-      method_name = CREATION_METHODS[:operator]
-    elsif CREATION_METHODS.key?(name)
-      method_name = CREATION_METHODS[name]
-    end
-    if method_name
-      nodes = send("create_#{method_name}", name, attributes, children_list)
+    case name
+    when DATA["paren"].method(:key?)
+      stretch_level = attributes["s"]
+      nodes = ZenmathBuilder.build_paren(name, stretch_level) do |content_element|
+        content_element << children_list[0]
+      end
+    when DATA["integral"].method(:key?)
+      nodes = ZenmathBuilder.build_integral(name) do |subscript_element, superscript_element|
+        subscript_element << children_list[0]
+        superscript_element << children_list[1]
+      end
+    when DATA["function"].method(:include?)
+      nodes = ZenmathBuilder.build_identifier(name, true)
+    when DATA["operator"].method(:key?)
+      symbol, kinds = DATA["operator"].fetch(name, [name, ["bin"]])
+      nodes = ZenmathBuilder.build_operator(symbol, kinds)
+    when "n"
+      number = children_list[0].first.to_s
+      nodes = ZenmathBuilder.build_number(number)
+    when "i"
+      name = children_list[0].first.to_s
+      nodes = ZenmathBuilder.build_identifier(name, false)
+    when "op"
+      name = children_list[0].first.to_s
+      nodes = ZenmathBuilder.build_identifier(name, true)
+    when "o"
+      name = children_list[0].first.to_s
+      nodes = ZenmathBuilder.build_operator(name, ["bin"])
+    when "sp"
+      nodes = ZenmathBuilder.build_superscript do |base_element, script_element|
+        base_element << children_list[0]
+        script_element << children_list[1]
+      end
+    when "sb"
+      nodes = ZenmathBuilder.build_subscript do |base_element, script_element|
+        base_element << children_list[0]
+        script_element << children_list[1]
+      end
+    when "sbsp"
+      nodes = ZenmathBuilder.build_subsuperscript do |base_element, subscript_element, superscript_element|
+        base_element << children_list[0]
+        subscript_element << children_list[1]
+        superscript_element << children_list[2]
+      end
+    when "frac"
+      nodes = ZenmathBuilder.build_fraction do |numerator_element, denominator_element|
+        numerator_element << children_list[0]
+        denominator_element << children_list[1]
+      end
+    when "sqrt"
+      stretch_level = attributes["s"]
+      nodes = ZenmathBuilder.build_radical(stretch_level) do |content_element|
+        content_element << children_list[0]
+      end
     end
     return nodes
   end
@@ -48,17 +78,14 @@ module ZenmathBuilder
     this = Nodes[]
     text.each_char do |char|
       if char =~ /\d/
-        this << Element.build("math-n") do |this|
-          this << Text.new(char, true, nil, false)
-        end
+        this << ZenmathBuilder.build_number(char)
       elsif char =~ /[[:alpha:]]/
-        this << Element.build("math-i") do |this|
-          this << Text.new(char, true, nil, false)
-        end
+        this << ZenmathBuilder.build_identifier(char, false)
       elsif char !~ /\s/
         name = DATA["operator"].find{|s, (t, u)| char == t}&.first || char
         name = DATA["replacement"].fetch(name, name)
-        this << create_operator(name, {}, [])
+        symbol, kinds = DATA["operator"].fetch(name, [name, ["bin"]])
+        this << ZenmathBuilder.build_operator(symbol, kinds)
       end
     end
     return this
@@ -72,170 +99,185 @@ module ZenmathBuilder
     return next_char
   end
 
-  def create_custom_number(name, attributes, children_list)
+  public
+
+  def self.build_number(number)
     this = Nodes[]
     this << Element.build("math-n") do |this|
-      this << children_list[0]
+      this << Text.new(number, true, nil, false)
     end
     return this
   end
 
-  def create_custom_identifier(name, attributes, children_list)
+  def self.build_identifier(name, function)
     this = Nodes[]
     this << Element.build("math-i") do |this|
-      this["class"] = "fun" unless name == "i"
-      this << children_list[0]
+      if function
+        this["class"] = "fun"
+      end
+      this << Text.new(name, true, nil, false)
     end
     return this
   end
 
-  def create_custom_operator(name, attributes, children_list)
+  def self.build_operator(symbol, kinds, &block)
     this = Nodes[]
     this << Element.build("math-o") do |this|
-      this << children_list[0]
-    end
-    return this
-  end
-
-  def create_operator(name, attributes, children_list)
-    this = Nodes[]
-    symbol, kinds = DATA["operator"].fetch(name, [name, ["bin"]])
-    clazz = kinds.join(" ")
-    this << Element.build("math-o") do |this|
-      this["class"] = clazz
+      this["class"] = kinds.join(" ")
       this << Text.new(symbol, true, nil, false)
     end
     return this
   end
 
-  def create_superscript(name, attributes, children_list)
+  def self.build_superscript(&block)
     this = Nodes[]
-    element_name = (name == "sp") ? "math-sup" : "math-sub"
-    this << Element.build(element_name) do |this|
+    base_element, script_element = nil
+    this << Element.build("math-sup") do |this|
       this << Element.build("math-base") do |this|
-        this << children_list[0]
+        base_element = this
       end
       this << Element.build("math-scr") do |this|
-        this << children_list[1]
+        script_element = this
       end
     end
+    block&.call(base_element, script_element)
     return this
   end
 
-  def create_subsuperscript(name, attributes, children_list)
+  def self.build_subscript(&block)
     this = Nodes[]
+    base_element, script_element = nil
+    this << Element.build("math-sub") do |this|
+      this << Element.build("math-base") do |this|
+        base_element = this
+      end
+      this << Element.build("math-scr") do |this|
+        script_element = this
+      end
+    end
+    block&.call(base_element, script_element)
+    return this
+  end
+
+  def self.build_subsuperscript(&block)
+    this = Nodes[]
+    base_element, subscript_element, superscript_element = nil
     this << Element.build("math-subsup") do |this|
       this << Element.build("math-base") do |this|
-        this << children_list[0]
+        base_element = this
       end
       this << Element.build("math-scr") do |this|
-        this << children_list[1]
+        subscript_element = this
       end
       this << Element.build("math-scr") do |this|
-        this << children_list[2]
+        superscript_element = this
       end
     end
+    block&.call(base_element, subscript_element, superscript_element)
     return this
   end
 
-  def create_fraction(name, attributes, children_list)
+  def self.build_fraction(&block)
     this = Nodes[]
+    numerator_element, denominator_element = nil
     this << Element.build("math-frac") do |this|
       this << Element.build("math-num") do |this|
-        this << children_list[0]
+        numerator_element = this
       end
       this << Element.build("math-denwrap") do |this|
         this << Element.build("math-den") do |this|
-          this << children_list[1]
+          denominator_element = this
         end
       end
     end
+    block&.call(numerator_element, denominator_element)
     return this
   end
 
-  def create_radical(name, attributes, children_list)
+  def self.build_radical(stretch_level, &block)
     this = Nodes[]
-    stretch = !attributes["s"]
-    stretch_level = attributes["s"] || "0"
-    symbol = DATA["radical"].fetch(stretch_level, "")
+    content_element = nil
     this << Element.build("math-sqrt") do |this|
       this << Element.build("math-surd") do |this|
-        this["class"] = "s#{stretch_level}" unless stretch
+        if stretch_level
+          this["class"] = "s#{stretch_level}" 
+          symbol = DATA["radical"].fetch(stretch_level, "")
+        else
+          symbol = DATA["radical"]["0"]
+        end
         this << Element.build("math-o") do |this|
           this << Text.new(symbol, true, nil, false)
         end
       end
       this << Element.build("math-sqrtcont") do |this|
-        this["class"] = "md-sqrt" if stretch
-        this << children_list[0]
+        unless stretch_level
+          this["class"] = "md-sqrt"
+        end
+        content_element = this
       end
     end
+    block&.call(content_element)
     return this
   end
 
-  def create_paren(name, attributes, children_list)
+  def self.build_paren(kind, stretch_level, &block)
     this = Nodes[]
-    stretch = !attributes["s"]
-    stretch_level = attributes["s"] || "0"
-    left_symbol, right_symbol = DATA["paren"][name].fetch(stretch_level, ["", ""])
+    content_element = nil
     this << Element.build("math-paren") do |this|
+      if stretch_level
+        left_symbol, right_symbol = DATA["paren"][kind].fetch(stretch_level, ["", ""])
+      else
+        left_symbol, right_symbol = DATA["paren"][kind].fetch("0", ["", ""])
+      end
       this << Element.build("math-o") do |this|
         this["class"] = "lp"
         this << Text.new(left_symbol, true, nil, false)
       end
       this << Element.build("math-parencont") do |this|
-        this["class"] = "md-paren md-paren-#{name}" if stretch
-        this << children_list[0]
+        unless stretch_level
+          this["class"] = "md-paren md-paren-#{kind}"
+        end
+        content_element = this
       end
       this << Element.build("math-o") do |this|
         this["class"] = "rp"
         this << Text.new(right_symbol, true, nil, false)
       end
     end
+    block&.call(content_element)
     return this
   end
 
-  def create_integral(name, attributes, children_list)
+  def self.build_integral(kind, &block)
     this = Nodes[]
-    symbol = DATA["integral"][name][1]
+    subscript_element, superscript_element = nil
     this << Element.build("math-subsup") do |this|
       this["class"] = "int"
       this << Element.build("math-base") do |this|
         this << Element.build("math-o") do |this|
           this["class"] = "int"
+          symbol = DATA["integral"][kind][1]
           this << Text.new(symbol, true, nil, false)
         end
       end
       this << Element.build("math-scr") do |this|
-        this << children_list[0]
+        subscript_element = this
       end
       this << Element.build("math-scr") do |this|
-        this << children_list[1]
+        superscript_element = this
       end
     end
+    block&.call(subscript_element, superscript_element)
     return this
   end
 
-  def create_function(name, attributes, children_list)
+  def self.build_text(&block)
     this = Nodes[]
-    this << Element.build("math-i") do |this|
-      this["class"] = "fun"
-      this << Text.new(name, true, nil, false)
-    end
-    return this
-  end
-
-  def create_raw_text(name, attributes, children_list)
-    this = Nodes[]
+    text_element = nil
     this << Element.build("math-text") do |this|
-      this << children_list[0]
+      text_element = this
     end
-    return this
-  end
-
-  def create_raw(name, attributes, children_list)
-    this = Nodes[]
-    this << children_list[0]
+    block&.call(text_element)
     return this
   end
 
