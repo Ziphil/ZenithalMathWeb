a2b79c73e7c039e04451e97eca7109d7259f9117
Ziphil <yfe23457@nifty.com>
2019-10-18 22:34:55 +0900

    リファクタリング

--- source/zenmath/resource/script/diagram.js
+++ source/zenmath/resource/script/diagram.js
@@ -10,103 +10,120 @@ const ARROW_TIP_SPECS = {
 class DiagramModifier extends Modifier {
 
   modify(element) {
-    let cellElements = Array.from(element.children).filter((child) => child.localName == "math-cellwrap").map((child) => child.children[0]);
     let arrowElements = Array.from(element.children).filter((child) => child.localName == "math-arrow");
+    let cellElements = Array.from(element.children).filter((child) => child.localName == "math-cellwrap").map((child) => child.children[0]);
     let graphic = this.createGraphic(element);
     element.appendChild(graphic);
     for (let arrowElement of arrowElements) {
-      let placeConfig = arrowElement.getAttribute("data-place");
-      let match;
-      if (match = placeConfig.match(/(\d+)(?:\.(\w+))?-(\d+)(?:\.(\w+))?/)) {
-        let startElement = cellElements[parseInt(match[1]) - 1];
-        let endElement = cellElements[parseInt(match[3]) - 1];
-        let startDimension = this.calcDimension(graphic, startElement);
-        let endDimension = this.calcDimension(graphic, endElement);
-        let startPosition = this.parsePosition(match[2]) || this.determinePosition(startDimension, endDimension);
-        let endPosition = this.parsePosition(match[4]) || this.determinePosition(endDimension, startDimension);
-        let arrow = this.createArrow(startDimension, endDimension, startPosition, endPosition);
-        graphic.appendChild(arrow);
-        let arrowDimension = this.calcDimension(graphic, arrowElement, graphic);
-        let startPoint = startDimension[startPosition];
-        let endPoint = endDimension[endPosition];
-        let basePoint = [(startPoint[0] + endPoint[0]) / 2, (startPoint[1] + endPoint[1]) / 2];
-        let angle = this.calcAngle(startPoint, endPoint) + Math.PI / 2;
-        if (arrowElement.getAttribute("inv")) {
-          angle += Math.PI;
-        }
-        if (angle > Math.PI) {
-          angle -= Math.PI * 2;
-        }
-        let point = this.calcLabelPoint(basePoint, arrowDimension, angle);
-        arrowElement.style.top = "" + (point.top / 0.8) + "em";
-        arrowElement.style.left = "" + (point.left / 0.8) + "em";
-      }
+      let arrowSpec = this.determineArrowSpec(graphic, arrowElement, cellElements);
+      let arrow = this.createArrow(arrowSpec);
+      graphic.appendChild(arrow);
+      let labelPoint = this.determineLabelPoint(graphic, arrowElement, arrowSpec);
+      let fontRatio = this.getFontSize(graphic) / this.getFontSize(arrowElement);
+      arrowElement.style.left = "" + (labelPoint[0] * fontRatio) + "em";
+      arrowElement.style.top = "" + (labelPoint[1] * fontRatio) + "em";
     }
   }
 
-  parsePosition(string) {
-    let position = null;
-    if (string == "west" || string == "w") {
-      position = "west";
-    } else if (string == "southwest" || string == "sw") {
-      position = "southWest";
-    } else if (string == "south" || string == "s") {
-      position = "south";
-    } else if (string == "southeast" || string == "se") {
-      position = "southEast";
-    } else if (string == "east" || string == "e") {
-      position = "east";
-    } else if (string == "northeast" || string == "ne") {
-      position = "northEast";
-    } else if (string == "north" || string == "n") {
-      position = "north";
-    } else if (string == "northwest" || string == "nw") {
-      position = "northWest";
+  determineArrowSpec(graphic, arrowElement, cellElements) {
+    let placeConfig = arrowElement.getAttribute("data-place");
+    let match = placeConfig.match(/(\d+)(?:\.(\w+))?-(\d+)(?:\.(\w+))?/);
+    if (match) {
+      let startElement = cellElements[parseInt(match[1]) - 1];
+      let endElement = cellElements[parseInt(match[3]) - 1];
+      let startDimension = this.calcDimension(graphic, startElement);
+      let endDimension = this.calcDimension(graphic, endElement);
+      let startDirection = this.parseDirection(match[2]) || this.determineDirection(startDimension, endDimension);
+      let endDirection = this.parseDirection(match[4]) || this.determineDirection(endDimension, startDimension);
+      let startPoint = startDimension[startDirection];
+      let endPoint = endDimension[endDirection];
+      return {startPoint, endPoint};
+    } else {
+      let startPoint = [0, 0];
+      let endPoint = [0, 0];
+      return {startPoint, endPoint};
     }
-    return position;
   }
 
-  determinePosition(baseDimension, destinationDimension) {
-    let angle = this.calcAngle(baseDimension.center, destinationDimension.center);
-    let position = "west";
-    for (let i = 0 ; i < 8 ; i ++) {
-      let firstAngle = (i == 0) ? -Math.PI : this.calcAngle(baseDimension.center, baseDimension[DIRECTIONS[i]]);
-      let secondAngle = (i == 7) ? Math.PI : this.calcAngle(baseDimension.center, baseDimension[DIRECTIONS[i + 1]]);
-      let maxAngle = (firstAngle + secondAngle) / 2;
-      if (angle <= maxAngle) {
-        position = DIRECTIONS[i];
-        break;
-      }
+  determineLabelPoint(graphic, labelElement, arrowSpec) {
+    let labelDimension = this.calcDimension(graphic, labelElement);
+    let startPoint = arrowSpec.startPoint;
+    let endPoint = arrowSpec.endPoint;
+    let basePoint = [(startPoint[0] + endPoint[0]) / 2, (startPoint[1] + endPoint[1]) / 2];
+    let angle = this.calcAngle(startPoint, endPoint) + Math.PI / 2;
+    if (labelElement.getAttribute("inv")) {
+      angle += Math.PI;
     }
-    return position;
+    if (angle > Math.PI) {
+      angle -= Math.PI * 2;
+    }
+    let point = this.calcLabelPoint(basePoint, labelDimension, angle);
+    return point;
   }
 
   calcLabelPoint(basePoint, labelDimension, angle) {
     let distance = 5 / 18;
     let epsilon = Math.PI / 90;
-    let position = "realEast";
+    let direction = "realEast";
     if (angle <= -Math.PI + epsilon) {
-      position = "realEast";
+      direction = "realEast";
     } else if (angle <= -Math.PI / 2 - epsilon) {
-      position = "realNorthEast";
+      direction = "realNorthEast";
     } else if (angle <= -Math.PI / 2 + epsilon) {
-      position = "realNorth";
+      direction = "realNorth";
     } else if (angle <= -epsilon) {
-      position = "realNorthWest";
+      direction = "realNorthWest";
     } else if (angle <= epsilon) {
-      position = "realWest";
+      direction = "realWest";
     } else if (angle <= Math.PI / 2 - epsilon) {
-      position = "realSouthWest";
+      direction = "realSouthWest";
     } else if (angle <= Math.PI / 2 + epsilon) {
-      position = "realSouth";
+      direction = "realSouth";
     } else if (angle <= Math.PI - epsilon) {
-      position = "realSouthEast";
+      direction = "realSouthEast";
     } else {
-      position = "realEast";
+      direction = "realEast";
     }
-    let top = basePoint[1] - Math.sin(angle) * distance + labelDimension.realNorthWest[1] - labelDimension[position][1];
-    let left = basePoint[0] + Math.cos(angle) * distance + labelDimension.realNorthWest[0] - labelDimension[position][0];
-    return {top, left};
+    let x = basePoint[0] + Math.cos(angle) * distance + labelDimension.realNorthWest[0] - labelDimension[direction][0];
+    let y = basePoint[1] - Math.sin(angle) * distance + labelDimension.realNorthWest[1] - labelDimension[direction][1];
+    return [x, y];
+  }
+
+  parseDirection(string) {
+    let direction = null;
+    if (string == "west" || string == "w") {
+      direction = "west";
+    } else if (string == "southwest" || string == "sw") {
+      direction = "southWest";
+    } else if (string == "south" || string == "s") {
+      direction = "south";
+    } else if (string == "southeast" || string == "se") {
+      direction = "southEast";
+    } else if (string == "east" || string == "e") {
+      direction = "east";
+    } else if (string == "northeast" || string == "ne") {
+      direction = "northEast";
+    } else if (string == "north" || string == "n") {
+      direction = "north";
+    } else if (string == "northwest" || string == "nw") {
+      direction = "northWest";
+    }
+    return direction;
+  }
+
+  determineDirection(baseDimension, destinationDimension) {
+    let angle = this.calcAngle(baseDimension.center, destinationDimension.center);
+    let direction = "west";
+    for (let i = 0 ; i < 8 ; i ++) {
+      let firstAngle = (i == 0) ? -Math.PI : this.calcAngle(baseDimension.center, baseDimension[DIRECTIONS[i]]);
+      let secondAngle = (i == 7) ? Math.PI : this.calcAngle(baseDimension.center, baseDimension[DIRECTIONS[i + 1]]);
+      let maxAngle = (firstAngle + secondAngle) / 2;
+      if (angle <= maxAngle) {
+        direction = DIRECTIONS[i];
+        break;
+      }
+    }
+    return direction;
   }
 
   calcAngle(baseCoords, destinationCoords) {
@@ -116,11 +133,11 @@ class DiagramModifier extends Modifier {
     return angle;
   }
 
-  createArrow(startDimension, endDimension, startPosition, endPosition) {
-    let startCoords = startDimension[startPosition];
-    let endCoords = endDimension[endPosition];
+  createArrow(arrowSpec) {
+    let startPoint = arrowSpec.startPoint;
+    let endPoint = arrowSpec.endPoint;
     let arrow = this.createSvgElement("path");
-    arrow.setAttribute("d", "M " + startCoords[0] + " " + startCoords[1] + " L " + endCoords[0] + " " + endCoords[1]);
+    arrow.setAttribute("d", "M " + startPoint[0] + " " + startPoint[1] + " L " + endPoint[0] + " " + endPoint[1]);
     arrow.setAttribute("marker-end", "url(#tip-normal)");
     return arrow;
   }
@@ -155,16 +172,16 @@ class DiagramModifier extends Modifier {
     return element;
   }
 
-  calcDimension(graphic, element, fontElement) {
+  calcDimension(graphic, element) {
     let dimension = {};
-    let fontSize = this.getFontSize(fontElement || element)
+    let fontSize = this.getFontSize(graphic)
     let graphicTop = graphic.getBoundingClientRect().top + window.pageYOffset;
     let graphicLeft = graphic.getBoundingClientRect().left + window.pageXOffset;
     let top = (element.getBoundingClientRect().top + window.pageYOffset - graphicTop) / fontSize;
     let left = (element.getBoundingClientRect().left + window.pageXOffset - graphicLeft) / fontSize;
-    let width = this.getWidth(element, fontElement);
-    let height = this.getHeight(element, fontElement);
-    let lowerHeight = this.getLowerHeight(element, fontElement);
+    let width = this.getWidth(element, graphic);
+    let height = this.getHeight(element, graphic);
+    let lowerHeight = this.getLowerHeight(element, graphic);
     let margin = 5 / 18;
     dimension.northWest = [left - margin, top - margin];
     dimension.north = [left + width / 2, top - margin];
