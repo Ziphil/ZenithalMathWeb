6088e47b4f5bd68b146e12129f6ca54308434df1
Ziphil <yfe23457@nifty.com>
2019-11-01 01:55:45 +0900

    添字の位置調整方法を変更

--- source/zenmath/resource/script/subsuper.js
+++ source/zenmath/resource/script/subsuper.js
@@ -9,25 +9,24 @@ class SubsuperModifier extends Modifier {
     let superElement = Array.from(element.children).find((child) => child.localName == "math-sup");
     let leftSubElement = Array.from(element.children).find((child) => child.localName == "math-lsub");
     let leftSuperElement = Array.from(element.children).find((child) => child.localName == "math-lsup");
-    let baseSymbolElement = Array.from(baseElement.children).find((child) => child.localName == "math-o");
-    let subWidth = (subElement) ? this.getWidth(subElement) : 0;
-    let superWidth = (superElement) ? this.getWidth(superElement) : 0;
-    let leftSubWidth = (leftSubElement) ? this.getWidth(leftSubElement) : 0;
-    let leftSuperWidth = (leftSuperElement) ? this.getWidth(leftSuperElement) : 0;
-    let subShift = (subElement) ? this.calcSubShift(baseElement, subElement, superElement) : 0;
-    let superShift = (superElement) ? this.calcSuperShift(baseElement, superElement, subElement) : 0;
-    let leftSubShift = (leftSubElement) ? this.calcSubShift(baseElement, leftSubElement, leftSuperElement) : 0;
-    let leftSuperShift = (leftSuperElement) ? this.calcSuperShift(baseElement, leftSuperElement, leftSubElement) : 0;
+    this.modifySide(element, baseElement, subElement, superElement, "right");
+    this.modifySide(element, baseElement, leftSubElement, leftSuperElement, "left");
+  }
+
+  modifySide(element, baseElement, subElement, superElement, side) {
+    let bothShifts = this.calcBothShifts(baseElement, subElement, superElement);
+    let subShift = bothShifts.sub;
+    let superShift = bothShifts.super;
     let subMargin = 0;
     let superMargin = (subElement) ? -this.getWidth(subElement) : 0;
-    let leftSubMargin = (leftSuperElement) ? -this.getWidth(leftSuperElement) : 0;
-    let leftSuperMargin = 0;
-    if (baseSymbolElement && element.classList.contains("int")) {
-      let difference = (element.classList.contains("inl")) ? 0.3 : 0.6
-      subWidth -= difference;
-      subMargin -= difference;
+    let subWidth = (subElement) ? this.getWidth(subElement) : 0;
+    let superWidth = (superElement) ? this.getWidth(superElement) : 0;
+    if (side == "right" && element.classList.contains("int")) {
+      let slope = (element.classList.contains("inl")) ? 0.3 : 0.6
+      subWidth -= slope;
+      subMargin -= slope;
       if (subElement) {
-        superMargin += difference;
+        superMargin += slope;
       }
     }
     if (subElement) {
@@ -38,45 +37,51 @@ class SubsuperModifier extends Modifier {
       superElement.style.verticalAlign = "" + superShift + "em";
       superElement.style.marginLeft = "" + superMargin + "em";
     }
-    if (leftSubElement) {
-      leftSubElement.style.verticalAlign = "" + leftSubShift + "em";
-      leftSubElement.style.marginRight = "" + leftSubMargin + "em";
-    }
-    if (leftSuperElement) {
-      leftSuperElement.style.verticalAlign = "" + leftSuperShift + "em";
-      leftSuperElement.style.marginRight = "" + leftSuperMargin + "em";
-    }
-    if (superElement && subWidth > superWidth) {
+    if (side == "right" && superElement && subWidth > superWidth) {
       superElement.style.width = "" + subWidth + "em";
     }
-    if (leftSubElement && leftSuperWidth > leftSubWidth) {
-      leftSubElement.style.width = "" + leftSuperWidth + "em";
+    if (side == "left" && subElement && superWidth > subWidth) {
+      subElement.style.width = "" + superWidth + "em";
     }
   }
 
-  calcSubShift(baseElement, subElement, superElement) {
-    let fontRatio = this.getFontSize(baseElement) / this.getFontSize(subElement);
-    let shiftConst = -0.3
-    if (superElement || (baseElement.parentNode.classList.contains("int") && !baseElement.parentNode.classList.contains("inl"))) {
-      shiftConst = -0.2
+  calcBothShifts(baseElement, subElement, superElement) {
+    let subShift = (subElement) ? this.calcSubShift(baseElement, subElement) : 0;
+    let superShift = (superElement) ? this.calcSuperShift(baseElement, superElement) : 0;
+    if (subElement && superElement) {
+      let subHeight = this.getHeight(subElement);
+      let gap = -subShift + superShift - subHeight;
+      if (gap < 0.2) {
+        subShift -= (0.2 - gap) / 2;
+        superShift += (0.2 - gap) / 2;
+      }
     }
+    return {sub: subShift, super: superShift};
+  }
+
+  calcSubShift(baseElement, subElement) {
+    let fontRatio = this.getFontSize(baseElement) / this.getFontSize(subElement);
     let height = this.getLowerHeight(baseElement);
     if (height < 0.4875) {
       height = 0.4875;
+    }
+    let shiftConst = -0.2;
+    if (baseElement.parentNode.classList.contains("int") && !baseElement.parentNode.classList.contains("inl")) {
+      shiftConst = -0.1;
     }
     let shift = (height + shiftConst) * fontRatio;
     return -shift;
   }
 
-  calcSuperShift(baseElement, superElement, subElement) {
+  calcSuperShift(baseElement, superElement) {
     let fontRatio = this.getFontSize(baseElement) / this.getFontSize(superElement);
-    let shiftConst = -0.2;
-    if (subElement || (baseElement.parentNode.classList.contains("int") && !baseElement.parentNode.classList.contains("inl"))) {
-      shiftConst = -0.1
-    }
     let height = this.getUpperHeight(baseElement);
     if (height < 0.5125) {
       height = 0.5125;
+    }
+    let shiftConst = -0.2;
+    if (baseElement.parentNode.classList.contains("int") && !baseElement.parentNode.classList.contains("inl")) {
+      shiftConst = -0.1;
     }
     let shift = (height + shiftConst) * fontRatio;
     return shift;
