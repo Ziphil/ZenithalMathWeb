5940920758c3d5a312d4a017a67686a326ab7eca
Ziphil <yfe23457@nifty.com>
2019-11-14 16:43:11 +0900

    証明図の罫線の長さの計算方法を変更

--- source/zotica/resource/script/tree.js
+++ source/zotica/resource/script/tree.js
@@ -11,14 +11,36 @@ class TreeModifier extends Modifier {
     let lastContentElement = this.calcContentElement(lastAntecedentElement);
     let leftLabelElement = element.previousElementSibling;
     let rightLabelElement = element.nextElementSibling;
-    let wholeWidth = this.getWidth(element);
-    let leftExtrusion = (this.getWidth(firstAntecedentElement) - this.getWidth(firstContentElement)) / 2;
-    let rightExtrusion = (this.getWidth(lastAntecedentElement) - this.getWidth(lastContentElement)) / 2;
+    let fontSize = this.getFontSize(element);
+    let fontRatio = fontSize / this.getFontSize(leftLabelElement);
+    let firstAntecedentWidth = this.getWidth(firstAntecedentElement);
+    let lastAntecedentWidth = this.getWidth(lastAntecedentElement);
+    let firstContentWidth = this.getWidth(firstContentElement);
+    let lastContentWidth = this.getWidth(lastContentElement);
     let leftLabelWidth = this.getWidth(leftLabelElement, element);
     let rightLabelWidth = this.getWidth(rightLabelElement, element);
+    let wholeWidth = this.getWidth(element);
+    let leftExtrusion = 0;
+    let rightExtrusion = 0;
+    if (firstContentElement.localName != "math-axiom") {
+      leftExtrusion = firstContentElement.offsetLeft / fontSize;
+    }
+    if (lastContentElement.localName != "math-axiom") {
+      rightExtrusion = (lastContentElement.offsetParent.offsetWidth - lastContentElement.offsetLeft - lastContentElement.offsetWidth) / fontSize;
+    }
     let lineWidth = wholeWidth - leftExtrusion - rightExtrusion;
-    antecendentParentElement.style.setProperty("margin-left", "" + (-leftExtrusion - leftLabelWidth) + "em", "important");
-    antecendentParentElement.style.setProperty("margin-right", "" + (-rightExtrusion - rightLabelWidth) + "em", "important");
+    let consequentElement = this.findChild(element, "math-denwrap");
+    let lineElement = this.findChild(consequentElement, "math-line");
+    lineElement.style.setProperty("width", "" + lineWidth + "em", "important");
+    lineElement.style.setProperty("margin-left", "" + leftExtrusion + "em", "important");
+    element.style.setProperty("margin-right", "" + (-rightExtrusion) + "em", "important");
+    if (rightLabelWidth < rightExtrusion) {
+      rightLabelElement.style.setProperty("margin-right", "" + ((rightExtrusion - rightLabelWidth) * fontRatio) + "em", "important");
+    }
+    element.style.setProperty("margin-left", "" + (-leftExtrusion) + "em", "important");
+    if (leftLabelWidth < leftExtrusion) {
+      leftLabelElement.style.setProperty("margin-left", "" + ((leftExtrusion - leftLabelWidth) * fontRatio) + "em", "important");
+    }
   }
 
   calcContentElement(antecedentElement) {
